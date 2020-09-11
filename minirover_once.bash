0121be71564aaae86b90b8388e35cd1f80ea98b5
Pito Salas <pitosalas@gmail.com>
2020-09-10 20:33:57 -0400

    more

--- minirover_once.bash
+++ minirover_once.bash
@@ -2,17 +2,21 @@
 
 echo "[running minirover_once.bash]"
 # Thanks to Bernardo
+
 echo 127.0.0.1 `hostname` >> /etc/hosts
+myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' ; }
 
 # Put here the local IP of the host device (robot)
 export LANIFACE=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 export HOST_IP=$(ifconfig ${LANIFACE} | awk '/inet / {print $2}')
 
+# uncomment to be on VPN
+export HOST_IP=$(myvpnip)
+
 export ROS_IP=${HOST_IP}
 echo export ROS_IP=${HOST_IP} >> ~/.bashrc
 
 # Default ROS master port is 11311
-#  - You could use 80 if accessing over the internet: roscore -p 80
 export ROS_MASTER_URI=http://${HOST_IP}:11311
 echo export ROS_MASTER_URI=http://${HOST_IP}:11311 >> ~/.bashrc
 
