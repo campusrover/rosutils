6ab62e8a0e10ef549da8cadba5834188f2367aad
Pito Salas <pitosalas@gmail.com>
2020-09-11 20:43:47 -0400

    tweak

--- clouddesktop_once.bash
+++ clouddesktop_once.bash
@@ -2,14 +2,15 @@
 echo "[running clouddesktop_once.bash]"
 myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' ; }
 export HOST_IP=$(myvpnip)
+export ROSCORE_IP=100.94.206.80
 
 export ROS_IP=${HOST_IP}
 echo export ROS_IP=${HOST_IP} >> ~/.bashrc
 
 # Default ROS master port is 11311
 #  - You could use 80 if accessing over the internet: roscore -p 80
-export ROS_MASTER_URI=http://${HOST_IP}:11311
-echo export ROS_MASTER_URI=http://${HOST_IP}:11311 >> ~/.bashrc
+export ROS_MASTER_URI=http://${ROSCORE_IP}:11311
+echo export ROS_MASTER_URI=http://${ROSCORE_IP}:11311 >> ~/.bashrc
 
 # Source ROS configuration
 echo "Sourcing ROS Melodic configuration..."
