92805461323520b0ab17ac68b06b9834042d6366
Pito Salas <pitosalas@gmail.com>
2020-09-11 21:05:06 -0400

    tweak

--- minirover_bashrc.bash
+++ minirover_bashrc.bash
@@ -121,29 +121,5 @@ fi
 
 # ROS Melodic & Workspace
 source /opt/ros/melodic/setup.bash
-echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++
-ROS Melodic is the current version"
-export EDITOR=nano
-
-#### WORKSPACE
-echo "ROS WORKSPACE: MiniRover"
-echo "ROS_IP is $ROS_IP and ROS_MASTER_URI is $ROS_MASTER_URI"
-source ~/catkin_ws/devel/setup.bash
-echo -e "and '~/catkin_ws' is the current Workspace
-  ++++++++++++++++++++++++++++++++++++++++++++++++++  "
-
-alias gazempty='roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch'
-alias teleop='roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch'
-alias settime='sudo ntpdate ntp.ubuntu.com'
-alias eb='nano ~/.bashrc'
-alias sb='source ~/.bashrc'
-alias gs='git status'
-alias gp='git pull'
-alias cw='cd ~/catkin_ws'
-alias cs='cd ~/catkin_ws/src'
-alias cm='cd ~/catkin_ws && catkin_make'
-
-stopnow() { rostopic pub /cmd_vel geometry_msgs/Twist '{ linear: { x: 0.0,  y: 0.0,  z: 0.0 }, angular: { x: 0.0,  y: 0.0, z: 0.0 } } ';  }
-myip() { ip route get 1.2.3.4 | awk '{print $7}'; }
-
-myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' ; }
+source ~/rosutils/common_alias.bash
+source ~/rosutils/reset_context.bash
---
+++ reset_context.bash
@@ -0,0 +1,10 @@
+echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++
+ROS Melodic is the current version"
+export EDITOR=nano
+
+#### WORKSPACE
+echo "ROS WORKSPACE"
+echo "ROS_IP is $ROS_IP and ROS_MASTER_URI is $ROS_MASTER_URI"
+source ~/catkin_ws/devel/setup.bash
+echo -e "and '~/catkin_ws' is the current Workspace
+  ++++++++++++++++++++++++++++++++++++++++++++++++++  "
