b5d63a24a067c1f885bed0710a0f07c90f1a821a
Pito Salas <pitosalas@gmail.com>
2020-09-14 12:04:41 -0400

    tweak

--- clouddesktop_once.bash
+++ clouddesktop_once.bash
@@ -3,14 +3,16 @@ echo "[running clouddesktop_once.bash]"
 myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' ; }
 myip() { ip route get 1.2.3.4 | awk '{print $7}'; }
 
+# uncomment to be on VPN or not
 export HOST_IP=$(myvpnip)
-export ROSCORE_IP=100.94.206.80
-# or
-# export ROSCORE_IP=$(myvpnip)
+#export HOST_IP=$(myip)
+
+ROSCORE_IP=100.94.206.80
 
 export ROS_IP=${HOST_IP}
 export ROS_HOSTNAME=${HOST_IP}
-echo export ROS_IP=${HOST_IP} >> ~/.bashrc
+echo export ROS_IP=$ROS_IP >> ~/.bashrc
+echo export ROS_HOSTNAME=$HOST_IP >> ~/.bashrc
 
 # Default ROS master port is 11311
 #  - You could use 80 if accessing over the internet: roscore -p 80
