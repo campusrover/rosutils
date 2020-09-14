eeb611947077a9ed39ef089587a2539879ba7074
Pito Salas <pitosalas@gmail.com>
2020-09-14 09:22:54 -0400

    tweak

--- reset_context.bash
+++ reset_context.bash
@@ -1,7 +1,7 @@
 source ~/catkin_ws/devel/setup.bash
-echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++
+echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "ROS_IP is $ROS_IP"
 echo "ROS_MASTER_URI is $ROS_MASTER_URI"
 echo "ROS_HOSTNAME is $ROS_HOSTNAME"
-echo -e "and '~/catkin_ws' is the current Workspace
-++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e "and '~/catkin_ws' is the current Workspace"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
