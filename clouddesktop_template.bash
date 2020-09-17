aa73286aed3cb2f07f881c98da698cada1a8acfd
Pito Salas <pitosalas@gmail.com>
2020-09-17 15:15:42 -0400

    tweak

--- clouddesktop_template.bash
+++ clouddesktop_template.bash
@@ -1,7 +1,7 @@
 # Remember to run `sudo -s source rosutils/minirover_once.bash` once!
 # It will add some lines to this file
 
-source ~/rosutils/common_alias
+source ~/rosutils/common_alias.bash
 source ~/rosutils/clouddesktop_setup.bash
-source ~/rosutils/rset cloud
+source ~/rosutils/rset.bash cloud
 
