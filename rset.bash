dc60114536aae61af563f5a622a872801817da52
Pito Salas <pitosalas@gmail.com>
2020-09-17 21:30:27 -0400

    added real link

--- rset.bash
+++ rset.bash
@@ -8,27 +8,29 @@ myip() { ip route get 1.2.3.4 | awk '{print $7}'; }
 myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' ; }
 
 echo [running rset.bash]
-if test "$1" == robot; then
+if test -z $1; then
+    source ~/rosutils/showenv.bash
+elif test "$1" == "robot"; then
     echo setting environment ROBOT
     export ROS_MASTER_URI=http://$ROBOT_IP:11311
     export ROS_IP=$(myvpnip)
     export SETSTATE=ROBOT
-elif test $1 == docker; then
+elif test "$1" == docker; then
     echo   [setting environment DOCKER]
     export ROS_MASTER_URI=http://$(myip):11311; 
     export ROS_IP=$(myip)
     export SETSTATE=docker
-elif test $1 == cloud; then
+elif test "$1" == cloud; then
     echo   [setting environment CLOUD]
     export ROS_MASTER_URI=http://$(myvpnip):11311
     export ROS_IP=$(myvpnip)
     export SETSTATE=CLOUD
-elif test $1 == pi; then
+elif test "$1" == pi; then
     echo   [setting environment pi]
     export ROS_MASTER_URI=http://$(myvpnip):11311
     export ROS_IP=$(myvpnip)
     export SETSTATE=PI
 else
     echo bad input to rset. Must be pi, robot, docker or cloud
+    exit 2
 fi
-
