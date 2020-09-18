a0a323dcac97c5ad2dee8f1e57fc178438b09949
Pito Salas <pitosalas@gmail.com>
2020-09-17 22:29:03 -0400

    small typo

--- common_alias.bash
+++ common_alias.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "[running common_alias.bash]"
 
-export ROBOT_IP=100.94.206.80
 
 # Bash Functions
 stopnow() { rostopic pub /cmd_vel geometry_msgs/Twist '{ linear: { x: 0.0,  y: 0.0,  z: 0.0 }, angular: { x: 0.0,  y: 0.0, z: 0.0 } } ';  }
 myip() { ip route get 1.2.3.4 | awk '{print $7}'; }
 myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' ; }
-rset() {source ~/rosutils/rset.bash "$1";}
+
+rset() {local cmd="$1"; source ~/rosutils/rset.bash "$cmd";}
 
 # Aliases
 alias gazempty='roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch'
--- rset.bash
+++ rset.bash
@@ -4,12 +4,17 @@
 #       docker - docker working with a simulated robot
 #       cloud - cloud working with simulated robot
 #       pi - on board actual robot
+# Enter by hand the known ip of the rapsberry pi on the robot
+export ROBOT_IP=100.94.206.80
+
+# Useful functions
 myip() { ip route get 1.2.3.4 | awk '{print $7}'; }
 myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' ; }
 
 echo [running rset.bash]
 if test -z $1; then
     source ~/rosutils/showenv.bash
+    exit
 elif test "$1" == "robot"; then
     echo setting environment ROBOT
     export ROS_MASTER_URI=http://$ROBOT_IP:11311
@@ -34,3 +39,4 @@ else
     echo bad input to rset. Must be pi, robot, docker or cloud
     exit 2
 fi
+source ~/rosutils/showenv.bash
