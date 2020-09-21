6f84c01ee1aaa8decd8f0837b6fa0c12a2d02ca5
Pito Salas <pitosalas@gmail.com>
2020-09-21 11:16:13 -0400

    development

--- showenv.bash
+++ showenv.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ~/catkin_ws/devel/setup.bash
+cd ~/catkin_ws
 echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "rset $SETSTATE"
 echo "ROS_IP is $ROS_IP"
@@ -7,4 +8,4 @@ echo "ROS_MASTER_URI is $ROS_MASTER_URI"
 echo "ROS_HOSTNAME is $ROS_HOSTNAME"
 echo -e "and '~/catkin_ws' is the current Workspace"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-cd ~/catkin_ws
+
