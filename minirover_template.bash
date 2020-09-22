4688ea1fec18694abb427cc5a30419036cbc46d7
Pito Salas <pitosalas@gmail.com>
2020-09-22 08:00:06 -0400

    development

--- minirover_template.bash
+++ minirover_template.bash
@@ -1,6 +1,3 @@
-# Remember to run `sudo -s source rosutils/minirover_once.bash` once!
-# It will add some lines to this file
-
 source ~/rosutils/common_alias.bash
 source ~/rosutils/clouddesktop_setup.bash
 
--- rset_current.bash
+++
@@ -1,4 +0,0 @@
-#source ~/rosutils/rset.bash pi
-source ~/rosutils/rset.bash robot
-#source ~/rosutils/rset.bash cloud
-#source ~/rosutils/rset.bash docker
