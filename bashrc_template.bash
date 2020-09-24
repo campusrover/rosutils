b11fb63a6cf37decb18a3c438630e50db29a9761
Pito Salas <pitosalas@gmail.com>
2020-09-24 12:31:12 -0400

    more

--- bashrc_template.bash
+++ bashrc_template.bash
@@ -1,2 +1,8 @@
 source ~/rosutils/common_alias.bash
-source ~/rosutils/rset_current.bash
+echo "********************************************"
+echo "FIRST TIME: Update rset_current.bash"
+echo "Replace this block of echos with the right choice based on your needs"
+echo "One of rset_robot, rset_cloud, rset_pi"
+echo "********************************************"
+
+
--- rset_current.bash
+++
@@ -1,7 +0,0 @@
-echo "********************************************"
-echo "FIRST TIME: Update rset_current.bash"
-echo "Replace this file with the right choice based on your needs"
-echo "One of rset_robot, rset_cloud, rset_pi"
-echo "********************************************"
-
-
