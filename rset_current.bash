fae615e3f0244c95116206a02d1618e09dbd3e62
Pito Salas <pitosalas@gmail.com>
2020-09-24 12:17:22 -0400

    more

--- rset_current.bash
+++ rset_current.bash
@@ -1,5 +1,7 @@
+echo "********************************************"
 echo "FIRST TIME: Update rset_current.bash"
 echo "Replace this file with the right choice based on your needs"
 echo "One of rset_robot, rset_cloud, rset_pi"
+echo "********************************************"
 
 
