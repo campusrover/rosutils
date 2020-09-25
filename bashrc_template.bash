ad6e055ffa522e3f5a20c55db10bb340387d80a3
Pito Salas <pitosalas@gmail.com>
2020-09-25 10:10:01 -0400

    more

--- bashrc_template.bash
+++ bashrc_template.bash
@@ -2,12 +2,18 @@ echo "[running ~/.bashrc]"
 source ~/rosutils/common_alias.bash
 echo "********************************************"
 echo "Copy this file to ~/.bashrc"
-echo "Uncomment the rset command below"
-echo "If want to access a physical robot, place its vpnip belows"
+echo "Uncomment the right rset command below"
+echo "If want to access a physical robot, place its vpnip below"
 echo "And delete these echo lines"
 echo "********************************************"
 
-# rset robot, cloud, pi, etc.
+# Uncomment one of these"
+# rset robot # if you are on your web desktop and want to control a real robot
+# rset cloud # if you are on your web desktiop and working with a simulated robot
+# rset pi # if this bashrc is on your raspberry pi
+
+# If you are working with a real robot (or are the real robot) uncomment
+# this line and put the vpnip of the robot
 # export ROBOT_IP=100.94.206.80
 
 
