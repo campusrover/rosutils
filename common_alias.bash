95f44db65000bea6863de10d5caf36843ec19601
Pito Salas <pitosalas@gmail.com>
2021-06-01 17:15:26 -0400

    sorted lines

--- common_alias.bash
+++ common_alias.bash
@@ -9,24 +9,24 @@ myvpnip() { ip addr show dev tailscale0 | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3
 rset() { source ~/rosutils/rset.bash "$1";}
 
 # Aliases
-alias restart='supervisorctl -u root -p dev@ros restart x:*'
-alias gazempty='roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch'
-alias teleop='roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch'
-alias settime='sudo ntpdate ntp.ubuntu.com'
-alias eb='nano ~/.bashrc'
-alias sb='source ~/.bashrc'
-alias gs='git status'
-alias gp='git pull'
-alias cw='cd ~/catkin_ws'
+alias cm='cd ~/catkin_ws && catkin_make'
 alias cs='cd ~/catkin_ws/src'
 alias cu='cd ~/rosutils'
-alias cm='cd ~/catkin_ws && catkin_make'
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
 alias cu='cd ~/rosutils'
+alias cw='cd ~/catkin_ws'
+alias eb='nano ~/.bashrc'
+alias gazempty='roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch'
+alias gp='git pull'
+alias gs='git status'
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
 alias lw='cd ~/linorobot_ws/; source devel/setup.bash'
 alias platformio='PATH=$PATH:~/.platformio/penv/bin platformio run --target upload'
+alias restart='supervisorctl -u root -p dev@ros restart x:*'
+alias sb='source ~/.bashrc'
+alias settime='sudo ntpdate ntp.ubuntu.com'
+alias teleop='roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch'
 
 # Prompt
 
