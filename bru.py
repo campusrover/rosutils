8c4888c59275ed6dbb02084cc99744bd016a9d8e
Pito Salas <pitosalas@gmail.com>
2022-02-11 14:39:25 -0500

    changes

--- bru.py
+++ bru.py
@@ -34,8 +34,8 @@ import time
 
 MODES = ['sim', 'real', 'onboard', 'labonboard']
 TYPES = ['tb3', 'minirover', 'bullet']
-ROBOTS = ['pitosalas', 'bullet1', 'robc', 'mr1', 'august', 'mr2']
-TYPE_MAP = {'pitosalas':'minirover', 'bullet1':'bullet', 'robc' : 'tb3', 'mr1': 'minirover', 'mr2' : 'minirover', 'august':'minirover'}
+ROBOTS = ['pitosalas', 'bullet1', 'robc', 'roba', 'robb', 'rafael', 'mr1', 'august', 'mr2']
+TYPE_MAP = {'pitosalas':'minirover', 'bullet1':'bullet', 'robc' : 'tb3', 'mr1': 'minirover', 'mr2' : 'minirover', 'august':'minirover', 'robb': 'tb3', 'roba' : 'tb3'}
 
 class Bru(object):
     def __init__(self):
