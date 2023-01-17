ebbf1f8631b9d52373c299c6bcd929caa4500785
Adam Ring <57344340+Aringan0323@users.noreply.github.com>
2023-01-17 14:01:38 -0500

    add alias to automatically ssh into real robot using BRU_MASTER_IP

--- common_alias.bash
+++ common_alias.bash
@@ -33,7 +33,7 @@ alias cameraon='roslaunch raspicam_node camerav2_1280x960_10fps.launch'
 alias real='$(bru mode real)'
 alias sim='$(bru mode sim)'
 alias bringup='roslaunch turtlebot3_bringup turtlebot3_robot.launch' # Will be 'roslaunch platform full_bringup.launch' if platform robot
-
+alias sshrobot='ssh ubuntu@$BRU_MASTER_IP'
 # Prompt
 
 PS1="[\$BRU_MODE:\$BRU_NAME]\w$ "
