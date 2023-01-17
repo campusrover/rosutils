76eff1990a92304d3cd5593f6eb39c085cbc5cb2
Adam Ring <adamring@brandeis.edu>
2023-01-17 22:43:16 +0000

    Added alias to automatically ssh into the selected robot through bru

--- common_alias.bash
+++ common_alias.bash
@@ -32,6 +32,7 @@ alias cmall='catkin_make -DCATKIN_WHITELIST_PACKAGES=""'
 alias cameraon='roslaunch raspicam_node camerav2_1280x960_10fps.launch'
 alias real='$(bru mode real)'
 alias sim='$(bru mode sim)'
+alias sshrobot='ssh ubuntu@$BRU_MASTER_IP'
 alias bringup='roslaunch turtlebot3_bringup turtlebot3_robot.launch' # Will be 'roslaunch platform full_bringup.launch' if platform robot
 
 # Prompt
