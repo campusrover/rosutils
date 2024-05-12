180f99a559ba34517d164089961465d21462cc22
Pito Salas <pitosalas@users.noreply.github.com>
2024-05-12 09:50:47 -0400

    update

--- measure_bandwidh.sh
+++ measure_bandwidh.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-echo "Topic \t\t Bandwidth"
+echo -e "Topic\t\tBandwidth"
 for topic in $(rostopic list)
 do
+    # Sanitize topic name to create a valid filename
+    safe_topic=$(echo "$topic" | tr -d '/' | tr ' ' '_')
+
     # Run rostopic bw in the background and capture PID
-    rostopic bw $topic > /tmp/topic_bw_$topic.txt 2>&1 &
+    rostopic bw $topic > "/tmp/topic_bw_${safe_topic}.txt" 2>&1 &
     PID=$!
     sleep 2  # Adjust the sleep time if necessary
     kill $PID
     wait $PID 2>/dev/null
 
     # Extract bandwidth information
-    bw_info=$(grep "average:" /tmp/topic_bw_$topic.txt | awk '{print $2 " " $3}')
+    bw_info=$(grep "average:" "/tmp/topic_bw_${safe_topic}.txt" | awk '{print $2 " " $3}')
     if [ ! -z "$bw_info" ]; then
-        echo -e "${topic} \t $bw_info"
+        echo -e "${topic}\t$bw_info"
     fi
 
     # Clean up
-    rm /tmp/topic_bw_$topic.txt
+    rm "/tmp/topic_bw_${safe_topic}.txt"
 done
