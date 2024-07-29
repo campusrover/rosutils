3e5d9b17d7f53a5a459186a8fcfbb9d39d4bc2d6
Pito Salas <pitosalas@gmail.com>
2024-07-29 21:41:15 +0000

    work in progress

--- install/pio-upload.sh
+++ install/pio-upload.sh
@@ -4,7 +4,7 @@ original_dir=$(pwd)
 
 cw
 roscd linorobot/teensy/firmware
-platformio run --target upload -e teensy40
+platformio run --target upload -e firmware40
 
 # Return to the original directory
 cd "$original_dir"
