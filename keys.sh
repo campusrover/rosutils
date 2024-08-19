6f40fdfd77d7fdb04b35a959c9e204fb832d88f4
Pito Salas <pitosalas@users.noreply.github.com>
2024-08-19 16:42:24 -0400

    update

--- keys.sh
+++ keys.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo ./pi_connect.sh tskey-auth-k37QNpiUmv11CNTRL-nF14ZpayUE3v4qdVd7a9G3v2XWG14xbg
-
-
+# sudo ./pi_connect.sh tskey-auth-k37QNpiUmv11CNTRL-nF14ZpayUE3v4qdVd7a9G3v2XWG14xbg
 #export k2="tskey-auth-kbFN4FNXLw11CNTRL-yPZywKcZBuRwZRXPm4uqkRnZxHLerqBN"
+sudo ./pi_connect.sh tskey-auth-kpz6CCGnxi11CNTRL-wzDkQZ69rUJhvoGnR5DVTJRiM5mzfBeUE
 
