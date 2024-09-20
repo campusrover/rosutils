3e9ca62fdede40ae85471f873eea0b076e1b4629
Pito Salas <pitosalas@gmail.com>
2024-09-20 18:19:00 +0000

    work in progress

--- keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# sudo ./pi_connect.sh tskey-auth-k37QNpiUmv11CNTRL-nF14ZpayUE3v4qdVd7a9G3v2XWG14xbg
-# sudo ./pi_connect.sh tskey-auth-kbFN4FNXLw11CNTRL-yPZywKcZBuRwZRXPm4uqkRnZxHLerqBN
-# sudo ./pi_connect.sh tskey-auth-kE9VK7U7eP11CNTRL-VMC2ti74H5ZbVbiQ3dn4yYZ1sQDLJ6gm
-
-
-sudo ./pi_connect.sh tskey-auth-kpz6CCGnxi11CNTRL-wzDkQZ69rUJhvoGnR5DVTJRiM5mzfBeUE
-
