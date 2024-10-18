6c093b2f25cb961b57f10d4246029a31f9df8f19
Pito Salas <pitosalas@users.noreply.github.com>
2024-10-18 19:34:41 -0400

    update

--- vnckey.sh
+++ vnckey.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Tailscale auth key. For VNC Reusable, non-ephemeral. Tagged tag:vnc
-sudo ./pi_connect.sh tskey-auth-kUP2wS4gz111CNTRL-dBueUXHYXA7BsEhiPCmV27miEpwTimPf
+sudo ./pi_connect.sh tskey-auth-kUP2wS4gz111CNTRL-dBueUXHYXA7BsEhiPCmV27miEpwTimPf tag:vnc
