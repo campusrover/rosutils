e9f0bf186b8f53856a15fe565131fe517619eebf
Pito Salas <pitosalas@gmail.com>
2024-12-19 13:14:40 -0500

    work in progress

--- ros2_robot_bashrc.bash
+++ ros2_robot_bashrc.bash
@@ -4,7 +4,7 @@ HISTSIZE=1000
 HISTFILESIZE=2000
 
 source /opt/ros/humble/setup.bash
-source ~/rosutils/ros2_common_bashrc
+source ~/rosutils/ros2_common_bashrc.bash
 
 export ROS_DOMAIN_ID=5
 export LINOROBOT2_BASE=2wd
@@ -12,4 +12,6 @@ export LINOROBOT2_LASER_SENSOR=a1
 export LINOROBOT2_DEPTH_SENSOR=
 
 source $HOME/linorobot2_ws/install/setup.bash
+alias bringup=ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0 --baudrate 921600
+
 
