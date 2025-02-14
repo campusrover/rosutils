adf473f44057755f139e45910816f9952f457266
Pito Salas <pitosalas@gmail.com>
2025-02-14 21:33:50 +0000

    work in progress

--- ros2_remote_bashrc.bash
+++ ros2_remote_bashrc.bash
@@ -3,7 +3,11 @@ echo "[running ~/rosutils/remote_bashrc]"
 HISTSIZE=1000
 HISTFILESIZE=2000
 
-export ROS2_WS=$HOME/ros2_ws
+export ROS2_WS=ros2_ws
 source ~/rosutils/ros2_common_bashrc.bash
 source /opt/ros/humble/setup.bash
+
+export LINOROBOT2_BASE=2wd
+export LINOROBOT2_LASER_SENSOR=
+export LINOROBOT2_DEPTH_SENSOR=
 
