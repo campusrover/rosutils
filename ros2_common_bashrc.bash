807633d0fa047c78efa66ff368f5ba0187334d2a
Pito Salas <pitosalas@gmail.com>
2025-12-05 10:51:17 -0500

    added BL_TUI_KEYBINDS

--- ros2_common_bashrc.bash
+++ ros2_common_bashrc.bash
@@ -3,3 +3,4 @@ echo "[running ~/rosutils/ros2_common_bashrc]"
 source ~/rosutils/ros2_common_alias.bash
 export PATH=$PATH:$HOME/.local/bin
 export PS1='[\h:\w] '
+export BL_TUI_KEYBINDS="exit: c-c; nodes: c-n; loglevel: c-l; mute: c-m;"
